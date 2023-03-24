fc62b52c9b8f844782aaab2f72207f74aee8a3bd
Harrison <103507184+hao441@users.noreply.github.com>
2023-03-24 20:54:09 +1100

    Bybit Requests

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 api_key=config.BYBIT_TEST_API_KEY
 secret_key=config.BYBTI_TEST_API_SECRET
 =======
@@ -16,16 +17,24 @@ def hello_world():
 api_key='XXXXXXXXX'
 secret_key='XXXXXXXXX'
 >>>>>>> 26bac21 (Bybit Code)
+=======
+api_key=config.BYBIT_TEST_API_KEY
+secret_key=config.BYBTI_TEST_API_SECRET
+>>>>>>> b41ee18 (Bybit Requests)
 httpClient=requests.Session()
 recv_window=str(5000)
 url="https://api-testnet.bybit.com" # Testnet endpoint
 
 def HTTP_Request(endPoint,method,payload,Info):
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b41ee18 (Bybit Requests)
 
 	global time_stamp
 	time_stamp=str(int(time.time() * 10 ** 3))
 	signature=genSignature(payload)
+<<<<<<< HEAD
 	if(method=="POST"):
 		headers = {
 			'X-BAPI-API-KEY': api_key,
@@ -37,11 +46,36 @@ def HTTP_Request(endPoint,method,payload,Info):
 		}
 	else:
 		headers = {
+=======
+	headers = {
+		'X-BAPI-API-KEY': api_key,
+		'X-BAPI-SIGN': signature,
+		'X-BAPI-SIGN-TYPE': '2',
+		'X-BAPI-TIMESTAMP': time_stamp,
+		'X-BAPI-RECV-WINDOW': recv_window,
+		'Content-Type': 'application/json'
+	}
+	if(method=="POST"):
+		response = httpClient.request(method, url+endPoint, headers=headers, data=payload)
+	else:
+		response = httpClient.request(method, url+endPoint+"?"+payload, headers=headers)
+	print(response.text)
+	print(Info + " Elapsed Time : " + str(response.elapsed))
+
+
+def HTTP_GET_Request(endPoint,method,payload,Info):
+
+	global time_stamp
+	time_stamp=str(int(time.time() * 10 ** 3))
+	signature=genSignature(payload)
+	headers = {
+>>>>>>> b41ee18 (Bybit Requests)
 		'X-BAPI-SIGN': signature,
 		'X-BAPI-API-KEY': api_key,
 		'X-BAPI-TIMESTAMP': time_stamp,
 		'X-BAPI-RECV-WINDOW': recv_window,
 		'cdn-request-id': 'test-001'
+<<<<<<< HEAD
 		}
 
 	if(method=="POST"):
@@ -257,24 +291,33 @@ def helloWorld():
         response = httpClient.request(method, url+endPoint+"?"+payload, headers=headers)
     print(response.text)
     print(Info + " Elapsed Time : " + str(response.elapsed))
+=======
+	}
+
+	response = httpClient.request(method, url+endPoint+"?accountType=CONTRACT&coin=USDT", headers=headers)
+	print(response.text)
+	print(Info + " Elapsed Time : " + str(response.elapsed))
+	return {"result": response.json()}
+>>>>>>> b41ee18 (Bybit Requests)
 
 def genSignature(payload):
-    param_str= str(time_stamp) + api_key + recv_window + payload
-    hash = hmac.new(bytes(secret_key, "utf-8"), param_str.encode("utf-8"),hashlib.sha256)
-    signature = hash.hexdigest()
-    return signature
+	param_str= str(time_stamp) + api_key + recv_window + payload
+	hash = hmac.new(bytes(secret_key, "utf-8"), param_str.encode("utf-8"),hashlib.sha256)
+	signature = hash.hexdigest()
+	return signature
 
-#Create Order
-endpoint="/contract/v3/private/order/create"
-method="POST"
-orderLinkId=uuid.uuid4().hex
-params='{"symbol": "BTCUSDT","side": "Buy","positionIdx": 1,"orderType": "Limit","qty": "0.001","price": "10000","timeInForce": "GoodTillCancel","orderLinkId": "' + orderLinkId + '"}'
-HTTP_Request(endpoint,method,params,"Create")
+def getWalletBalance():
+	endpoint="/v5/account/wallet-balance"
+	method="GET"
+	params="accountType=CONTRACT&coin=USDT"
+	response = HTTP_GET_Request(endpoint,method,params,"Balance")
+	print(response)
+	return response
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-	data = json.loads(request.data)
+def setLeverage():
+	return
 
+<<<<<<< HEAD
 	if data['pass'] != config.WEBHOOK_PASS:
 		return {
 			"code":"error",
@@ -286,3 +329,73 @@ def webhook():
 		"message":"success"
 	}
 >>>>>>> 26bac21 (Bybit Code)
+=======
+def checkPrice():
+	return
+
+def placeOrder(symbol, side, quantity):
+    # #Create Order
+    endpoint="/v5/order/create"
+    method="POST"
+    orderLinkId=uuid.uuid4().hex
+    preParams={"category":"linear","symbol":symbol,"side":side,"orderType":"Market","qty":quantity,"timeInForce":"IOC","positionIdx":0}
+    postParams=json.dumps(preParams)
+    response = HTTP_Request(endpoint,method,postParams,"Create")
+    print(response)
+    return {'response': response}
+
+def closePosition(symbol, side):
+	endpoint="/v5/order/create"
+	method="POST"
+	direction = "Sell" if side == "Buy" else "Buy"
+	print(f'direction is {direction}')
+	preParams = {"category":"linear","symbol":symbol,"side":direction,"orderType":"Market","qty":"0","timeInForce":"IOC","positionIdx":0, "reduceOnly":"true"}
+	postParams = json.dumps(preParams)
+	response = HTTP_Request(endpoint,method,postParams,"Create")
+	return {'response': response}
+
+def cancelAllOrders():
+    endpoint="/v5/order/cancel-all"
+    method="POST"
+    params='{"category":"linear","settleCoin":"BTC"}'
+    response = HTTP_Request(endpoint,method,params,"Cancel-all")
+    return {'response': response}
+
+@app.route('/cancelall', methods=['POST'])
+def cancelAllOrders():
+    endpoint="/v5/order/cancel-all"
+    method="POST"
+    params='{"category":"linear","settleCoin":"BTC"}'
+    response = HTTP_Request(endpoint,method,params,"Cancel-all")
+    return {'response': response}
+
+@app.route('/walletbalance', methods=['GET'])
+def walletBalance():
+	response = getWalletBalance()
+	print(response)
+	return response
+
+@app.route('/placelong', methods=['POST'])
+def placeLong():
+	longResponse = placeOrder("BTCUSDT", "Buy", "0.01")
+	return {"result": longResponse}
+
+@app.route('/placeshort', methods=['POST'])
+def placeShort():
+    shortResponse = placeOrder("BTCUSDT", "Sell", "0.01")
+    print(shortResponse)
+    return {"result": shortResponse}
+
+@app.route('/closelong', methods=['POST'])
+def closeLong():
+	cancelAllOrders()
+	closeLongResponse = closePosition("BTCUSDT", "Buy")
+	print(f'closeLongResponse: {closeLongResponse}')
+	return {'response': closeLongResponse}
+
+@app.route('/closeshort', methods=['POST'])
+def closeShort():
+	cancelAllOrders()
+	closeShortResponse = closePosition("BTCUSDT", "Sell")
+	return {'response': closeShortResponse}
+>>>>>>> b41ee18 (Bybit Requests)
