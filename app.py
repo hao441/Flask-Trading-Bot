a29982df36ca42ccd07bbc6eea734eddf597dc62
Harrison <103507184+hao441@users.noreply.github.com>
2023-03-24 20:54:09 +1100

    Bybit Requests

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 api_key=config.BYBIT_TEST_API_KEY
 secret_key=config.BYBTI_TEST_API_SECRET
 =======
@@ -32,6 +33,10 @@ secret_key=config.BYBTI_TEST_API_SECRET
 >>>>>>> a115d0c (test)
 =======
 >>>>>>> f3c3397 (Bybit Code)
+=======
+api_key=config.BYBIT_TEST_API_KEY
+secret_key=config.BYBTI_TEST_API_SECRET
+>>>>>>> 9cf92d7 (Bybit Requests)
 httpClient=requests.Session()
 recv_window=str(5000)
 url="https://api-testnet.bybit.com" # Testnet endpoint
@@ -41,14 +46,18 @@ def HTTP_Request(endPoint,method,payload,Info):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> b41ee18 (Bybit Requests)
 =======
 >>>>>>> a115d0c (test)
+=======
+>>>>>>> 9cf92d7 (Bybit Requests)
 
 	global time_stamp
 	time_stamp=str(int(time.time() * 10 ** 3))
 	signature=genSignature(payload)
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -67,6 +76,8 @@ def HTTP_Request(endPoint,method,payload,Info):
 		headers = {
 <<<<<<< HEAD
 =======
+=======
+>>>>>>> 9cf92d7 (Bybit Requests)
 	headers = {
 		'X-BAPI-API-KEY': api_key,
 		'X-BAPI-SIGN': signature,
@@ -75,6 +86,7 @@ def HTTP_Request(endPoint,method,payload,Info):
 		'X-BAPI-RECV-WINDOW': recv_window,
 		'Content-Type': 'application/json'
 	}
+<<<<<<< HEAD
 =======
 >>>>>>> c818673 (Bot Complete and Operational)
 	if(method=="POST"):
@@ -88,6 +100,11 @@ def HTTP_Request(endPoint,method,payload,Info):
 		}
 	else:
 <<<<<<< HEAD
+=======
+	if(method=="POST"):
+		response = httpClient.request(method, url+endPoint, headers=headers, data=payload)
+	else:
+>>>>>>> 9cf92d7 (Bybit Requests)
 		response = httpClient.request(method, url+endPoint+"?"+payload, headers=headers)
 	print(response.text)
 	print(Info + " Elapsed Time : " + str(response.elapsed))
@@ -99,17 +116,21 @@ def HTTP_GET_Request(endPoint,method,payload,Info):
 	time_stamp=str(int(time.time() * 10 ** 3))
 	signature=genSignature(payload)
 	headers = {
+<<<<<<< HEAD
 >>>>>>> b41ee18 (Bybit Requests)
 =======
 		headers = {
 >>>>>>> c818673 (Bot Complete and Operational)
 =======
 >>>>>>> a115d0c (test)
+=======
+>>>>>>> 9cf92d7 (Bybit Requests)
 		'X-BAPI-SIGN': signature,
 		'X-BAPI-API-KEY': api_key,
 		'X-BAPI-TIMESTAMP': time_stamp,
 		'X-BAPI-RECV-WINDOW': recv_window,
 		'cdn-request-id': 'test-001'
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -335,12 +356,15 @@ def helloWorld():
     print(Info + " Elapsed Time : " + str(response.elapsed))
 <<<<<<< HEAD
 =======
+=======
+>>>>>>> 9cf92d7 (Bybit Requests)
 	}
 
 	response = httpClient.request(method, url+endPoint+"?accountType=CONTRACT&coin=USDT", headers=headers)
 	print(response.text)
 	print(Info + " Elapsed Time : " + str(response.elapsed))
 	return {"result": response.json()}
+<<<<<<< HEAD
 >>>>>>> b41ee18 (Bybit Requests)
 =======
 		}
@@ -397,24 +421,27 @@ def setLeverage(leverage):
 
 <<<<<<< HEAD
 =======
+=======
+>>>>>>> 9cf92d7 (Bybit Requests)
 
 def genSignature(payload):
-    param_str= str(time_stamp) + api_key + recv_window + payload
-    hash = hmac.new(bytes(secret_key, "utf-8"), param_str.encode("utf-8"),hashlib.sha256)
-    signature = hash.hexdigest()
-    return signature
+	param_str= str(time_stamp) + api_key + recv_window + payload
+	hash = hmac.new(bytes(secret_key, "utf-8"), param_str.encode("utf-8"),hashlib.sha256)
+	signature = hash.hexdigest()
+	return signature
 
-#Create Order
-endpoint="/contract/v3/private/order/create"
-method="POST"
-orderLinkId=uuid.uuid4().hex
-params='{"symbol": "BTCUSDT","side": "Buy","positionIdx": 1,"orderType": "Limit","qty": "0.001","price": "10000","timeInForce": "GoodTillCancel","orderLinkId": "' + orderLinkId + '"}'
-HTTP_Request(endpoint,method,params,"Create")
+def getWalletBalance():
+	endpoint="/v5/account/wallet-balance"
+	method="GET"
+	params="accountType=CONTRACT&coin=USDT"
+	response = HTTP_GET_Request(endpoint,method,params,"Balance")
+	print(response)
+	return response
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-	data = json.loads(request.data)
+def setLeverage():
+	return
 
+<<<<<<< HEAD
 >>>>>>> f3c3397 (Bybit Code)
 	if data['pass'] != config.WEBHOOK_PASS:
 		return {
@@ -438,22 +465,40 @@ def checkPrice():
 	return response
 
 def placeOrder(side, quantity, stopLoss, takeProfit):
+=======
+def checkPrice():
+	return
+
+def placeOrder(symbol, side, quantity):
+>>>>>>> 9cf92d7 (Bybit Requests)
     # #Create Order
     endpoint="/v5/order/create"
     method="POST"
     orderLinkId=uuid.uuid4().hex
+<<<<<<< HEAD
     preParams={"category":"linear","symbol":"BTCUSDT","side":side,"orderType":"Market","qty":quantity,"timeInForce":"IOC","positionIdx":0, "stopLoss": stopLoss, "takeProfit":takeProfit}
+=======
+    preParams={"category":"linear","symbol":symbol,"side":side,"orderType":"Market","qty":quantity,"timeInForce":"IOC","positionIdx":0}
+>>>>>>> 9cf92d7 (Bybit Requests)
     postParams=json.dumps(preParams)
     response = HTTP_Request(endpoint,method,postParams,"Create")
     print(response)
     return {'response': response}
 
+<<<<<<< HEAD
 def closePosition(side):
+=======
+def closePosition(symbol, side):
+>>>>>>> 9cf92d7 (Bybit Requests)
 	endpoint="/v5/order/create"
 	method="POST"
 	direction = "Sell" if side == "Buy" else "Buy"
 	print(f'direction is {direction}')
+<<<<<<< HEAD
 	preParams = {"category":"linear","symbol":"BTCUSDT","side":direction,"orderType":"Market","qty":"0","timeInForce":"IOC","positionIdx":0, "reduceOnly":"true"}
+=======
+	preParams = {"category":"linear","symbol":symbol,"side":direction,"orderType":"Market","qty":"0","timeInForce":"IOC","positionIdx":0, "reduceOnly":"true"}
+>>>>>>> 9cf92d7 (Bybit Requests)
 	postParams = json.dumps(preParams)
 	response = HTTP_Request(endpoint,method,postParams,"Create")
 	return {'response': response}
@@ -465,6 +510,7 @@ def cancelAllOrders():
     response = HTTP_Request(endpoint,method,params,"Cancel-all")
     return {'response': response}
 
+<<<<<<< HEAD
 def getPosition():
 	endpoint="/v5/position/list"
 	method="GET"
@@ -550,19 +596,51 @@ def placeShort():
 	tradingStopResponse = tradingStopUpdate(updatedStopLoss,updatedTakeProfit)
 
 	return {"placeOrderResponse":longResponse, "tradingStopResponse":tradingStopResponse}
+=======
+@app.route('/cancelall', methods=['POST'])
+def cancelAllOrders():
+    endpoint="/v5/order/cancel-all"
+    method="POST"
+    params='{"category":"linear","settleCoin":"BTC"}'
+    response = HTTP_Request(endpoint,method,params,"Cancel-all")
+    return {'response': response}
+
+@app.route('/walletbalance', methods=['GET'])
+def walletBalance():
+	response = getWalletBalance()
+	print(response)
+	return response
+
+@app.route('/placelong', methods=['POST'])
+def placeLong():
+	longResponse = placeOrder("BTCUSDT", "Buy", "0.01")
+	return {"result": longResponse}
+
+@app.route('/placeshort', methods=['POST'])
+def placeShort():
+    shortResponse = placeOrder("BTCUSDT", "Sell", "0.01")
+    print(shortResponse)
+    return {"result": shortResponse}
+>>>>>>> 9cf92d7 (Bybit Requests)
 
 @app.route('/closelong', methods=['POST'])
 def closeLong():
 	cancelAllOrders()
+<<<<<<< HEAD
 	closeLongResponse = closePosition("Buy")
 	
 	# time.sleep(5)
 
+=======
+	closeLongResponse = closePosition("BTCUSDT", "Buy")
+	print(f'closeLongResponse: {closeLongResponse}')
+>>>>>>> 9cf92d7 (Bybit Requests)
 	return {'response': closeLongResponse}
 
 @app.route('/closeshort', methods=['POST'])
 def closeShort():
 	cancelAllOrders()
+<<<<<<< HEAD
 <<<<<<< HEAD
 	closeShortResponse = closePosition("BTCUSDT", "Sell")
 	return {'response': closeShortResponse}
@@ -584,3 +662,7 @@ def helloWorld():
 =======
 	}
 >>>>>>> f3c3397 (Bybit Code)
+=======
+	closeShortResponse = closePosition("BTCUSDT", "Sell")
+	return {'response': closeShortResponse}
+>>>>>>> 9cf92d7 (Bybit Requests)
